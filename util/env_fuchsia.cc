011043113ed311e65b73313b313949be34ec15a4
Przemyslaw Pietrzkiewicz <ppi@google.com>
2016-09-20 18:03:49 +0200

    FuchsiaEnv: don't include the local time in logger.
    Local time API is not currently available on Fuchsia.
    
    Change-Id: I65ebabd0bd532802f0175ae1a36087e6b1b66ef7

--- util/env_fuchsia.cc
+++ util/env_fuchsia.cc
@@ -84,17 +84,8 @@ class FuchsiaLogger : public Logger {
 
       struct timeval now_tv;
       gettimeofday(&now_tv, NULL);
-      const time_t seconds = now_tv.tv_sec;
-      struct tm t;
-      localtime_r(&seconds, &t);
       p += snprintf(p, limit - p,
-                    "%04d/%02d/%02d-%02d:%02d:%02d.%06d %llx ",
-                    t.tm_year + 1900,
-                    t.tm_mon + 1,
-                    t.tm_mday,
-                    t.tm_hour,
-                    t.tm_min,
-                    t.tm_sec,
+                    "%06d %llx ",
                     static_cast<int>(now_tv.tv_usec),
                     static_cast<long long unsigned int>(thread_id));
 
